8e1e6355e9811a06cf282221e09b8dd043015d9b
Niek Raaijmakers <raaijmak@adobe.com>
2021-07-12 05:33:25 +0200

    Add viewport

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ export default function Index({ posts, preview }) {
       <Layout preview={preview}>
         <Head>
           <title>{PAGE_TITLE}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </Head>
         <TopContainer/>
         <HeroBanner />
