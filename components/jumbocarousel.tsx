2c2fec89e2636b4420d20a4a068a7a61b081ddf2
Niek Raaijmakers <raaijmak@adobe.com>
2021-08-03 06:37:12 +0200

    Switch to react swiper

--- components/jumbocarousel.tsx
+++ components/jumbocarousel.tsx
@@ -4,40 +4,24 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {CarouselModel, getCarouselItemsById} from "../lib/carousel";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import SwiperCore, {
+    Pagination
+} from 'swiper/core';
+SwiperCore.use([Pagination]);
 
 
-const items = [
-    {
-        img: 'https://media.istockphoto.com/photos/woman-working-out-at-home-picture-id1203043225',
-        title: 'First slide label',
-        description: 'Test1',
-        posCssClasses: 'top left '
-    },
-    {
-        img: 'https://media.istockphoto.com/photos/beautiful-happy-girl-with-closed-eyes-practicing-yoga-in-lotus-in-picture-id1184595440',
-        title: 'First slide label',
-        description: 'Test2',
-        posCssClasses:  'x-center y-center odd'
-    },
-    {
-        img: 'https://media.istockphoto.com/photos/having-a-laugh-while-limbering-up-picture-id962529334',
-        title: 'First slide label',
-        description: 'Test3',
-        posCssClasses: 'bottom right'
-    }
-]
-
-export interface JumboCarouselProps {
-    id:string
-}
 
 function JumboCarousel(props:CarouselModel) {
 
     return (
-        <Carousel className={"my-4 jumbo-carousel"} interval={null} >
+
+        <Swiper pagination={true} className="mySwiper">
             {
                 props.items.map((item, index) =>
-                    <Carousel.Item key={'carousel-' + index} className={'mh-100'} >
+                    <SwiperSlide key={"swiperslide="+ index}>
 
                         {
                             item.backgroundImage && (
@@ -47,43 +31,41 @@ function JumboCarousel(props:CarouselModel) {
                                         layout={"fill"}
                                         objectFit={'cover'}
                                         src={item.backgroundImage.url}
-                                        />
+                                    />
                                 </div>
                             )
                         }
 
-                        <Carousel.Caption className={item.cssLocation}>
-                            <Container>
-                                <Row xs={12}>
-                                    <Col>
-                                        <h3>{item.heading}</h3>
-                                    </Col>
-                                </Row>
-                                <Row xs={12}>
-                                    <Col>
-                                        <div dangerouslySetInnerHTML={{__html: item.description.html}}/>
-                                    </Col>
-                                </Row>
+                        <Container className={"carousel-caption " + item.cssLocation}>
+                            <Row xs={12}>
+                                <Col>
+                                    <h3>{item.heading}</h3>
+                                </Col>
+                            </Row>
+                            <Row xs={12}>
+                                <Col>
+                                    <div dangerouslySetInnerHTML={{__html: item.description.html}}/>
+                                </Col>
+                            </Row>
 
-                                {
-                                    !!item.cta && (
-                                        <Row xs={12}>
-                                            <Col>
-                                                <Button href={item.cta.url} variant={item.cta.isSecondary ? 'secondary' : 'primary'} >
-                                                    <span>{item.cta.text}</span>
-                                                </Button>
-                                            </Col>
-                                        </Row>
-                                    )
-                                }
+                            {
+                                !!item.cta && (
+                                    <Row xs={12}>
+                                        <Col>
+                                            <Button href={item.cta.url} variant={item.cta.isSecondary ? 'secondary' : 'primary'} >
+                                                <span>{item.cta.text}</span>
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                )
+                            }
 
-                            </Container>
+                        </Container>
 
-                        </Carousel.Caption>
-                    </Carousel.Item>
+                    </SwiperSlide>
                 )
             }
-        </Carousel>
+         </Swiper>
 
     );
 }
